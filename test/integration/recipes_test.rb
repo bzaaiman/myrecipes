8b7d7c6789b9fa38193ea240b2b48d73a4617851
Ben Zaaiman <ben.zaaiman@gmail.com>
2017-06-06 10:35:35 +0000

    Refactor

--- test/integration/recipes_test.rb
+++ test/integration/recipes_test.rb
@@ -29,6 +29,7 @@ class RecipesTest < ActionDispatch::IntegrationTest
     assert_match @recipe.chef.chefname, response.body
     assert_select "a[href=?]", edit_recipe_path(@recipe), text: "Edit this recipe"
     assert_select "a[href=?]", recipe_path(@recipe), text: "Delete this recipe"
+    assert_select "a[href=?]", recipes_path, text: "List recipes"
   end
   
   test "should accept valid recipe" do
